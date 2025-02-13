ade41e780830c32df864a753b2b50ba812b40e29
Septian Hadi Nugroho <septianhadinugroho4@gmail.com>
2025-02-13 09:49:09 +0700

    correct spelling of 'handleSearch' in SearchBar component

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { FaMagnifyingGlass } from 'react-icons/fa6'
 import { IoMdClose } from 'react-icons/io'
 
-const SearchBar = ({ value, onChange, handleSeach, onClearSearch }) => {
+const SearchBar = ({ value, onChange, handleSearch, onClearSearch }) => {
   return (
     <div className='w-80 flex items-center px-4 bg-slate-200 rounded-md'>
       <input
@@ -19,7 +19,7 @@ const SearchBar = ({ value, onChange, handleSeach, onClearSearch }) => {
         />
       )}
 
-      <FaMagnifyingGlass className='text-slate-500 cursor-pointer hover:text-black' onClick={handleSeach} />
+      <FaMagnifyingGlass className='text-slate-500 cursor-pointer hover:text-black' onClick={handleSearch} />
     </div>
   )
 }
